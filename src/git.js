0cf4fd878d15a01b4a80de824c615bf66b9053da
Beto Toro <beto.toro@zemoga.com>
2023-03-05 21:30:52 -0500

    feat: create the basis of the command line

--- src/git.js
+++ src/git.js
@@ -1,14 +1,24 @@
-import { exec } from "node:child_process"
-import { promisify } from "node:util"
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
 
 const exectAsync = promisify(exec)
 
-function cleanStdout(stdout) {
+function cleanStdout (stdout) {
   return stdout.trim().split('\n').filter(Boolean)
 }
 
-export async function getChangedFiles() {
+export async function getChangedFiles () {
   const { stdout } = await exectAsync('git status --porcelain')
   return cleanStdout(stdout)
     .map(line => line.split(' ').at(-1))
+}
+
+export async function getStagedFiles () {
+  const { stdout } = await exectAsync('git diff --cached --name-only')
+  return cleanStdout(stdout)
+}
+
+export async function getCommit ({ commit } = {}) {
+  const { stdout } = await exectAsync(`git commit -m ${commit}`)
+  return cleanStdout(stdout)
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,66 @@
-import { intro, outro, text, select } from '@clack/prompts'
+import { intro, outro, text, select, confirm } from '@clack/prompts'
 import { COMMIT_TYPES } from './commit-types.js'
-import { getChangedFiles } from './git.js'
+import { getChangedFiles, getStagedFiles, getCommit } from './git.js'
 import { trytm } from '@bdsqqq/try'
 import colors from 'picocolors'
 
 const [changedFiles, errorChangedFiled] = await trytm(getChangedFiles())
+const [stagedFiles, errorStagedFiles] = await trytm(getStagedFiles())
 
 intro(
   colors.inverse(`Asistente para la creación de commits por ${colors.yellow('@betotorodev')}`)
 )
 
-if (errorChangedFiled) {
+if (errorChangedFiled ?? errorStagedFiles) {
   outro(colors.red('Error: comprueba que tienes un repositorio de git'))
   process.exit(1)
 }
 
-// const consoleMessage = await text({
-//   message: colors.cyan('Introduce el mensaje del commit'),
-//   placeholder: 'Add new feature, etc'
-// })
+if (stagedFiles.length === 0) {
+  outro(colors.red('Error: No hay archivos que comitear'))
+  process.exit(1)
+}
+
+const commitMessage = await text({
+  message: colors.cyan('Introduce el mensaje del commit'),
+  placeholder: 'Add new feature, etc'
+})
 
 const commitType = await select({
   message: colors.cyan('Selecciona el tipo de commit'),
   options: Object.entries(COMMIT_TYPES).map(([key, value]) => ({
     value: key,
-    label: `${value.emoji} ${key} - ${value.description}`
+    label: `${value.emoji} ${key.padEnd(8, ' ')} - ${value.description}`
   }))
 })
+
+const { emoji, realese } = COMMIT_TYPES[commitType]
+
+let breakingChange = false
+if (realese) {
+  breakingChange = await confirm({
+    initialValue: false,
+    message: `¿Tiene este commit cambios que rompen la compatibilidad anterior?
+      ${colors.gray('Si la respuesta es sí, debeerías crear un commit con el tipo "BREAKING CHANGE" y al hacer release se publicará una versión mejor')}
+    `
+  })
+}
+
+let commit = `${emoji} ${commitType}: ${commitMessage}`
+commit = breakingChange ? `${commit} [BREAKING CHANGE]` : commit
+
+const shouldConfirm = await confirm({
+  initialValue: true,
+  message: `¿Quieres crear el commit con el siguiente mensaje? 
+  ${colors.green(colors.bold(commit))}
+  
+  ¿Confirmas?
+  `
+})
+
+if (!shouldConfirm) {
+  outro(colors.yellow('No se creo el commit'))
+  process.exit(0)
+}
 
 outro('Gracias por usar el asistente')
